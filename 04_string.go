dac79cdf7a3acd43ac2814113b452f26be16cd89
Vespri <kresna.vespri21@gmail.com>
2022-03-02 22:33:19 +0700

    Add file helloworld and 3 data type

---
+++ 04_string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Kresna")
+	fmt.Println("Kresna Vespri")
+	fmt.Println("Kresna Vespri Wijaya")
+
+	fmt.Println(len("Kresna"))
+	fmt.Println("Kresna Vespri"[0])
+	fmt.Println("Kresna Vespri Wijaya"[5])
+}
